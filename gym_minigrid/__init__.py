87871e66c5ee06fde95de0499b4f2d692b6d4ae1
Raymond Chua <ray.r.chua@gmail.com>
2025-06-03 17:47:33 -0400

    Import additional modules for enhanced accessibility

--- gym_minigrid/__init__.py
+++ gym_minigrid/__init__.py
@@ -3,3 +3,7 @@ from .envs import *
 
 # Import wrappers so it's accessible when installing with pip
 from .wrappers import *
+from .register import *
+from .rendering import *
+from .roomgrid import *
+from .window import *
