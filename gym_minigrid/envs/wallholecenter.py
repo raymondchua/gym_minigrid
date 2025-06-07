ea4f77fa74e88873edef50cf8f540a846a4252be
Raymond Chua <ray.r.chua@gmail.com>
2025-06-07 19:13:07 -0400

    Update mission description to refer to the green box instead of the yellow box

--- gym_minigrid/envs/wallholecenter.py
+++ gym_minigrid/envs/wallholecenter.py
@@ -146,7 +146,7 @@ class WallHoleCenterEnv(MiniGridEnv):
         else:
             self.place_agent()
 
-        self.mission = "get to the yellow box"
+        self.mission = "get to the green box"
 
     def _gen_grid_custom(
         self, width, height, custom_agent_pos=None, custom_agent_dir=None
@@ -217,7 +217,7 @@ class WallHoleCenterEnv(MiniGridEnv):
         else:
             self.place_agent()
 
-        self.mission = "get to the yellow goal square"
+        self.mission = "get to the green goal square"
 
     def _reward(self):
         """
